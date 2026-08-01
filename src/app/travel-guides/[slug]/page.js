612708fb595fbb36aa4b15efccb55f282425d247
AnuragA1727 <anurag.arora0810@gmail.com>
2026-08-02 01:21:05 +0530

    changed services bg image and optimized whatsapp button on travel guide card

--- src/app/travel-guides/[slug]/page.js
+++ src/app/travel-guides/[slug]/page.js
@@ -1,5 +1,6 @@
 import { notFound } from "next/navigation";
 import Image from "next/image";
+import siteConfig from "@/config/siteConfig";
 
 import { guides } from "@/data/travelGuides";
 
@@ -284,7 +285,7 @@ export default async function GuidePage({ params }) {
           <p>{guide.cta.description}</p>
 
           <a
-            href="https://wa.me/91998893198"
+            href= {siteConfig.whatsapp}
             target="_blank"
             rel="noopener noreferrer"
           >
